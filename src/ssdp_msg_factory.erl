73b94d23dff2a01c58392597d760f6f4d61164f8
Garth Hitchens <garth@spoke.net>
2014-03-23 17:22:16 -0700

    various minor fixes for jrc

--- src/ssdp_msg_factory.erl
+++ src/ssdp_msg_factory.erl
@@ -47,7 +47,7 @@ build_bye_bye(NT) ->
 build_msearch_response(ST, Uri, Service_Type) ->
     List = ["HTTP/1.1 200 OK", ?CRLF,
             "CACHE-CONTROL: max-age = 1200", ?CRLF,
-#           "DATE: ", get_date(), ?CRLF,
+%           "DATE: ", get_date(), ?CRLF,
             "EXT:", ?CRLF,
             "LOCATION: http://", ssdp_root_device:get_ip_port(), Uri,?CRLF,
             "ST: " , ST, ?CRLF,
