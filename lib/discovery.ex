22c9297d0301913daf6dad245b67bc6b5ec14a53
Chris Dutton <chris@rosepointnav.com>
2015-04-23 19:19:23 -0700

    Removed unnecessary config dir. Updated start methods to be more useful. Now can start with supervision correctly

--- lib/discovery.ex
+++ lib/discovery.ex
@@ -8,12 +8,24 @@ defmodule Discovery do
   @default_port Application.get_env :discovery, :port, 80
   @default_uri Application.get_env :discovery, :uri, "/jrtp/"
 
-  def start(args \\ []), do: :ssdp.start(merge_args(args))
+  @doc "Start SSDP and SSDP Root Device erlang modules"
+  def start(args \\ []), do: :ssdp.start_all(merge_args(args))
 
-  def start_all(args \\ []), do: :ssdp.start_all(merge_args(args))
+  @doc """
+  Starts SSDP and SSDP Root Device erlang modules with link to celling process
+  """
+  def start_link(args \\ []) do
+    :ssdp_root_device.start_link(merge_args(args))
+    :ssdp.start_link(merge_args(args))
+  end
 
-  def start_root(args \\ []), do: :ssdp_root_device.start(merge_args(args))
+  @doc "Start SSDP erlang module"
+  def start_ssdp(args \\ []), do: :ssdp.start(merge_args(args))
 
+  @doc "Start SSDP Root device erlang module"
+  def start_root_device(args \\ []), do: :ssdp_root_device.start(merge_args(args))
+
+  # Merges the passed arguments with the defaults
   defp merge_args(args) do
     usn = Dict.get(args, :usn, @default_usn)
     port = Dict.get(args, :port, @default_port)
